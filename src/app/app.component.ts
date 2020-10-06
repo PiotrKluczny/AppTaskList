02be507fc2970f0bab4b93f809a4a9d1046239f8
Piotr Kluczny <pk.kluczny@gmail.com>
2020-10-06 12:51:14 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID, Inject } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Task } from './task';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,98 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo-list-App';
+
+  taskName = 'Nowe zadań';
+  taskDate = '';
+  editMode = false;
+
+config: {[key: string]: string} = null;
+
+tasks: Task[] = [
+  {
+    name: 'Sprzątanie',
+    deadline: new Date().toDateString(),
+    done: false,
+  },
+  {
+    name: 'Mycie',
+    deadline: new Date().toDateString(),
+    done: false,
+  },
+  {
+    name: 'Pranie',
+    deadline: new Date().toDateString(),
+    done: true,
+  },
+  {
+    name: 'Odkurzanie',
+    deadline: new Date().toDateString(),
+    done: false,
+  }
+];
+
+constructor(){
+
+setTimeout( () => {
+  this.config = {
+    title: 'Lista zadań',
+    footer: '© Lista zadań z obiektu config',
+    date: new Date().toDateString(),
+  };
+}, 500 )
+this.sortTask();
+;}
+
+clearTasks(){
+  this.tasks = [];
 }
+
+createTask( ){
+  const task: Task = {
+    name: this.taskName,
+    deadline: this.taskDate,
+    done: false,
+  };
+  this.tasks.push(task);
+
+  this.taskName = '';
+  this.taskDate = '';
+  this.sortTask();
+}
+
+switchEditMode(){
+this.editMode = !this.editMode;
+}
+
+markTaskDone(task: Task){
+  task.done = true;
+  this.sortTask();
+}
+
+deleteTask(task: Task){
+  this.tasks = this.tasks.filter(e =>e !=task);
+  this.sortTask();
+}
+
+private sortTask(){
+  this.tasks = this.tasks.sort((a: Task, b: Task) =>
+  a.done === b.done ? 0 : a.done ? 1 : -1);
+}
+
+}
+
+
+
+
+  // getfooter(): string {
+  //   return '© Lista zadań, All rights reserved';
+  // }
+
+  // getdate(): Date {
+  //   return new Date();
+  // }
+
+  // constructor(@Inject(LOCALE_ID) private localId: string) {
+  //   console.log('Localization:', this.localId);
+  // }
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import localePl from '@angular/common/locales/pl';
+import { registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+registerLocaleData(localePl);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
